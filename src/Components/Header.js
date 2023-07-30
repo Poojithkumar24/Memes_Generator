395df401afec52c94c0e4dc9217f21147f185871
poojith kumar <poojithk956@gmail.com>
2023-07-30 16:48:29 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src=".\img\meme.png" 
+                className="header--image"
+            />
+           
+        </header>
+    )
+}
